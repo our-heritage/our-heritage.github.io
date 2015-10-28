91519d7a7d170c2ff9cdef5b51ea7a737cdf3ce7
karo-801chan <karolina-chan@hotmail.com>
2015-10-28 09:47:40 -0600

    Agrego el archivo creditos.html y los estilos de creditos, ademas de los archivos de grunt

---
+++ Gruntfile.js
@@ -0,0 +1,69 @@
+'use strict';
+
+module.exports = function (grunt) {
+    require('load-grunt-tasks')(grunt);
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        // Watch for changes and trigger compass
+        watch: {
+            sass: {
+                files: ['sass/**/*.{scss,sass}','sass/partials/**/*.{scss,sass}'],
+                tasks: ['sass:dist']
+            },
+            js: {
+                files: ['js/**/*.js'],
+                tasks: ['uglify:dev']
+            }
+        },
+
+        // Compass and scss
+        sass: {
+            options: {
+                sourceMap: true,
+                outputStyle: 'nested'
+            },
+            dist: {
+                files: {
+                    'css/styles.css': 'sass/styles.scss'
+                }
+            },
+            prod: {
+                options: {
+                  outputStyle: 'compressed'
+                },
+                files: {
+                    'css/styles.css': 'sass/styles.scss'
+                }
+            }
+        },
+
+        uglify: {
+          dev: {
+            options: {
+              mangle: false,
+              compress: false,
+              preserveComments: 'all',
+              beautify: true
+            },
+            files: {
+              'js/main.min.js': ['js/main.js']
+              // Output: input
+            }
+          },
+          prod: {
+            options: {
+              mangle: true,
+              sourceMap: true,
+              compress: true
+            },
+            files: {
+              'js/main.min.js': ['js/main.js']
+            }
+          }
+        }
+    });
+
+    grunt.registerTask('default', ['sass:dist', 'watch', 'uglify:dev']);
+    grunt.registerTask('prod', ['sass:prod', 'uglify:prod']);
+};
